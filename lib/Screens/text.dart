b65cfa17d426c86d276a1f6b0ee1e759d0bf2126
Angel Bista <anzilbista123@gmail.com>
2024-04-14 10:55:28 +0545

    first commit

---
+++ lib/Screens/text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class first extends StatelessWidget {
+  const first({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello Bro'),
+    );
+  }
+}
---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:ai_assistant/Screens/text.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: first(),
+    );
+  }
+}
