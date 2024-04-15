15aad4e84f788440bd91b9570dc1935b22356d46
Angel Bista <anzilbista123@gmail.com>
2024-04-15 23:03:27 +0545

    On boarding Screen Completed Fully Functionally

--- lib/Screens/onboarding_screen.dart
+++ lib/Screens/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ai_assistant/model/onboard.dart';
+import 'package:ai_assistant/screens/home_screen.dart';
 import 'package:ai_assistant/utils/global.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -7,80 +9,120 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final c = PageController();
+    final list = [
+      //onboarding 1
+      Onboard(
+        title: 'Ask me Anything',
+        lottie: 'ai_ask_me',
+        subtitle: "I'm here for you, If you need help simply you can ask me",
+      ),
+
+      //onboarding 2
+      Onboard(
+        title: 'Imagination to Reality',
+        lottie: 'ai_play',
+        subtitle:
+            'Just Imagine anything & let me know, I will create something wonderful for you!',
+      ),
+    ];
+
     return Scaffold(
-      body: Column(
-        children: [
-          //Lottie
-          Lottie.asset('assets/lottie/ai_ask_me.json', height: mq.height * .6),
+      body: PageView.builder(
+        controller: c,
+        itemCount: list.length,
+        itemBuilder: (ctx, ind) {
+          final islast = ind == list.length - 1;
+          return Column(
+            children: [
+              //Lottie
+              Lottie.asset('assets/lottie/${list[ind].lottie}.json',
+                  height: mq.height * .6, width: islast ? mq.width * 7 : null),
 
-          // Title
+              // Title
 
-          const Text(
-            'Ask me Anything',
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.w900, letterSpacing: .5),
-          ),
-
-          //for adding some space
-          SizedBox(
-            height: mq.height * .015,
-          ),
-
-          //subtitle
-          SizedBox(
-            width: mq.width * .8,
-            child: const Text(
-              "I'm here for you, If you need help simply you can ask me",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 13.5,
-                letterSpacing: .5,
-                color: Colors.black54,
+              Text(
+                list[ind].title,
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: .5),
               ),
-            ),
-          ),
 
-          const Spacer(),
+              //for adding some space
+              SizedBox(
+                height: mq.height * .015,
+              ),
 
-          //dots
-
-          Wrap(
-            spacing: 10,
-            children: List.generate(
-              2,
-              (i) => Container(
-                width: 10,
-                height: 8,
-                decoration: const BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
+              //subtitle
+              SizedBox(
+                width: mq.width * .8,
+                child: Text(
+                  list[ind].subtitle,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 13.5,
+                    letterSpacing: .5,
+                    color: Colors.black54,
                   ),
                 ),
               ),
-            ),
-          ),
 
-          const Spacer(),
+              const Spacer(),
 
-          //button
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                shape: const StadiumBorder(),
-                elevation: 0,
-                backgroundColor: const Color.fromARGB(255, 95, 143, 224),
-                textStyle:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                minimumSize: Size(mq.width * .4, 50)),
-            onPressed: () {},
-            child: const Text(
-              'Next',
-            ),
-          ),
-          const Spacer(
-            flex: 2,
-          ),
-        ],
+              //dots
+
+              Wrap(
+                spacing: 10,
+                children: List.generate(
+                  list.length,
+                  (i) => Container(
+                    width: i == ind ? 18 : 10,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color: i == ind ? Colors.blue : Colors.grey,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const Spacer(),
+
+              //button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    elevation: 0,
+                    backgroundColor: const Color.fromARGB(255, 95, 143, 224),
+                    textStyle: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w500),
+                    minimumSize: Size(mq.width * .4, 50)),
+                onPressed: () {
+                  if (islast) {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (_) => const HomeScreen(),
+                      ),
+                    );
+                  } else {
+                    c.nextPage(
+                        duration: const Duration(milliseconds: 600),
+                        curve: Curves.ease);
+                  }
+                },
+                child: Text(
+                  islast ? 'Finish' : 'Next',
+                ),
+              ),
+              const Spacer(
+                flex: 2,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
